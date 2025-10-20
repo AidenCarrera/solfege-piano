1d0e85113ffe2684b50ac8c79b7b71a302196cba
Aiden Carrera <aiden.carrera05@gmail.com>
2025-10-20 12:17:21 -0500

    Refine animation

--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -135,7 +135,7 @@ export default function Piano() {
       />
 
       <div
-        className="relative flex flex-col items-center transition-transform duration-[500ms] ease-in-out"
+        className="relative flex flex-col items-center piano-scale-transition"
         style={{
           transform: `scale(${pianoScale})`,
           transformOrigin: "top center",
